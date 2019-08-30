fac31058a9e9513a1a80ccba7326ad810575ed6f
mk-atlassian <47995834+mk-atlassian@users.noreply.github.com>
2019-08-30 12:52:52 +1000

    Add smol boiz version

---
+++ setup-beeeg-E-smolz.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+INFO="[INFO] BEEEGMOJIS: "
+WARNING="[WARNING] BEEEGMOJIS: "
+ERROR="[ERROR] BEEEGMOJIS: "
+
+APP="app"
+APP_ASAR="app.asar"
+APP_ASAR_BAK="app.asar.bak"
+SSB_INTEROP="${APP}/dist/ssb-interop.bundle.js"
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+
+
+echo "${INFO}Figuring out where Slack is installed"
+if [[ $OSTYPE == *"linux"* ]]; then
+    SLACK_DIR="/usr/lib/slack/resources/"
+    if [ -d "${SLACK_DIR}" ]; then
+        echo "${INFO}Assuming slack is at ${SLACK_DIR}"
+    else
+        echo "${ERROR}Slack is not at ${SLACK_DIR} , please modify this script with the correct path using where and following the link"
+        exit 3
+    fi
+elif [[ $OSTYPE == *"darwin"* ]]; then
+    SLACK_DIR="/Applications/Slack.app/Contents/Resources/"
+else
+    echo "${ERROR}You're using an unsupported operating system, exiting"
+    exit 1
+fi
+
+
+
+echo "${INFO}Making sure that NPM is installed"
+if ! npm version; then
+    if [[ $OSTYPE == *"linux"* ]]; then
+        echo "${ERROR}NodeJS not found, please install it"
+
+    elif [[ $OSTYPE == *"darwin"* ]]; then
+        read -p "${WARNING}NodeJS not found, is it alright to install it? [y/n] " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo "${WARNING}NodeJS not found, checking if Homebrew is installed"
+
+            if brew -v; then
+                echo "${INFO}Homebrew found"
+            else
+                echo "${WARNING}Homebrew not found, installing (you may have to rerun this script in a new terminal)"
+                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            fi
+
+            echo "${INFO}Installing NodeJS"
+            brew install node
+        fi
+    fi
+fi
+
+
+
+if [ -d "${SLACK_DIR}${APP}" ]; then
+    echo "${INFO}Cleaning up after last installation"
+    rm -r "${SLACK_DIR}${APP}"
+fi
+
+if [ -f "${SLACK_DIR}${APP_ASAR}" ]; then
+    echo "${INFO}Slack update found"
+    mv "${SLACK_DIR}${APP_ASAR}" "${SLACK_DIR}${APP_ASAR_BAK}"
+fi
+
+echo "${INFO}Unpacking Slack app"
+if [ -f "${SLACK_DIR}${APP_ASAR_BAK}" ]; then
+    mv "${SLACK_DIR}${APP_ASAR_BAK}" "${SLACK_DIR}${APP_ASAR}"
+fi
+if npx asar extract "${SLACK_DIR}${APP_ASAR}" "${SLACK_DIR}${APP}" ; then
+    echo "${INFO}Backing up packaged Slack"
+    mv "${SLACK_DIR}${APP_ASAR}" "${SLACK_DIR}${APP_ASAR_BAK}"
+else
+    echo "${ERROR}Extracting Slack app failed, cleaning up"
+    rm -rf "${SLACK_DIR}${APP}"
+    exit 2
+fi
+
+
+
+cat "${SCRIPT_DIR}/electron-hack/1.txt" >> "${SLACK_DIR}${SSB_INTEROP}"
+cat "${SCRIPT_DIR}/common/slack-beeeg-E-smolz.css" >> "${SLACK_DIR}${SSB_INTEROP}"
+cat "${SCRIPT_DIR}/electron-hack/2.txt" >> "${SLACK_DIR}${SSB_INTEROP}"
+
+echo "${INFO}Done, have fun!"
