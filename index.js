51442d90bf21dd68205db135222c653017194aad
Michael Kemp <mkemp@atlassian.com>
2020-05-13 15:43:24 +1000

    add BEEEEEG options to node scipt

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 
 const os = require('os');
 const { exec } = require('child_process');
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [{ name: 'BEEEEEG', alias: 'b', type: Boolean }]
+const commandLineOptions = commandLineArgs(optionDefinitions)
+
+if (commandLineOptions.BEEEEEG) {
+    console.info("I like 'em BEEEEEG\nhttps://youtu.be/WJ1I-z0pBU0")
+}
 
 if (os.type() === 'Windows_NT') {
-    exec('setup-beeeg-E-smolzbat');
+    if (commandLineOptions.BEEEEEG) {
+        exec('setup-beeeg-E-smolz.bat')
+    } else {
+        exec('setup.bat')
+    }
 } else {
-    exec("sh setup-beeeg-E-smolz.sh");
+    if (commandLineOptions.BEEEEEG) {
+        exec("sh setup-beeeg-E-smolz.sh")
+    } else {
+        exec("sh setup.sh")
+    }
 }
