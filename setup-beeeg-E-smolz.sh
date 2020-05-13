56163321a62039459bf4dbf03fe1bb2dc898801e
Michael Kemp <mkemp@atlassian.com>
2020-05-13 21:32:07 +1000

    Reuse node script in linux/darwin script

--- setup-beeeg-E-smolz.sh
+++ setup-beeeg-E-smolz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-STYLES_FILE="${SCRIPT_DIR}/common/slack-beeeg-E-smolz.css"
+STYLES_FILE="-b"
 MAIN_SCRIPT="setup.sh"
 
 ("$SCRIPT_DIR/$MAIN_SCRIPT" "$STYLES_FILE")
