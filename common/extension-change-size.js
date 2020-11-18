2141d3a006868c71692cbafc7ae64677d84affae
Michael Kemp <mkemp@atlassian.com>
2020-11-19 02:40:03 +1100

    Prettify

--- common/extension-change-size.js
+++ common/extension-change-size.js
@@ -1,37 +1,38 @@
-const linkToInjectedStyles = document.createElement('link');
-linkToInjectedStyles.id = 'slack-beeegmoji-styles';
-linkToInjectedStyles.rel = 'stylesheet';
-linkToInjectedStyles.type = 'text/css';
+const linkToInjectedStyles = document.createElement("link");
+linkToInjectedStyles.id = "slack-beeegmoji-styles";
+linkToInjectedStyles.rel = "stylesheet";
+linkToInjectedStyles.type = "text/css";
 document.head.appendChild(linkToInjectedStyles);
 
 const updateStyle = (size) => {
-    console.log(`applying style: ${size}`);
+  console.log(`applying style: ${size}`);
 
-    const smolStylesUrl = chrome.runtime.getURL('/slack-smolbois.css');
-    const beeegEsmolzStylesUrl = chrome.runtime.getURL('/slack-beeeg-E-smolz.css');
-    const beeegStylesUrl = chrome.runtime.getURL('/slack-beeegmojis.css');
+  const smolStylesUrl = chrome.runtime.getURL("/slack-smolbois.css");
+  const beeegEsmolzStylesUrl = chrome.runtime.getURL(
+    "/slack-beeeg-E-smolz.css"
+  );
+  const beeegStylesUrl = chrome.runtime.getURL("/slack-beeegmojis.css");
 
-    switch (size) {
-        case 'smolbois':
-            linkToInjectedStyles.href = smolStylesUrl;
-            break;
-        case 'beeegmojis':
-            linkToInjectedStyles.href = beeegStylesUrl;
-            break;
-        case 'beeeg-E-smolz':
-        default:
-            linkToInjectedStyles.href = beeegEsmolzStylesUrl;
-            break;
-    }
-}
+  switch (size) {
+    case "smolbois":
+      linkToInjectedStyles.href = smolStylesUrl;
+      break;
+    case "beeegmojis":
+      linkToInjectedStyles.href = beeegStylesUrl;
+      break;
+    case "beeeg-E-smolz":
+    default:
+      linkToInjectedStyles.href = beeegEsmolzStylesUrl;
+      break;
+  }
+};
 
 browser.storage.onChanged.addListener((changes, area) => {
-    if (area === 'local' && 'size' in changes) {
-        updateStyle(changes.size.newValue);
-    }
+  if (area === "local" && "size" in changes) {
+    updateStyle(changes.size.newValue);
+  }
 });
 
-browser.storage.local.get('size')
-    .then(result => updateStyle(result.size));
+browser.storage.local.get("size").then((result) => updateStyle(result.size));
 
-console.log('loading')
+console.log("loading");
